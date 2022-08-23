5ac72cf6ffe3a108164dd15f228e2254158baaad
Sebastian <66321717+Chsebas98@users.noreply.github.com>
2022-08-22 22:02:24 -0500

    Categoria en Home tarjeta creada

--- lib/src/viewModels/categorias_listado.dart
+++ lib/src/viewModels/categorias_listado.dart
@@ -14,13 +14,35 @@ List<Widget> categoriasListado(BuildContext context) {
 
 Widget _imagenCategoria(BuildContext context, Map<String, dynamic> categoria) {
   return GestureDetector(
-    onTap: () {
-      Navigator.pushNamed(context, "categoria");
-    },
-    child: Container(
-      child: Image(
-        image: NetworkImage(categoria['foto']),
-      ),
-    ),
-  );
+      onTap: () {
+        Navigator.pushNamed(context, "categoria");
+      },
+      child: Stack(
+        children: [
+          //imagen configuración
+          Container(
+              width: 130.0,
+              height: 100.0,
+              margin: const EdgeInsets.only(right: 10.0, top: 5.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image(
+                    image: NetworkImage(categoria["foto"]),
+                    fit: BoxFit.cover,
+                  ))),
+          //texto dentro de la imagen configuración
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.only(left: 0, bottom: 15.0),
+            child: Text(
+              categoria["nombre"],
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  decorationColor: Colors.black,
+                  decorationStyle: TextDecorationStyle.wavy),
+            ),
+          )
+        ],
+      ));
 }
