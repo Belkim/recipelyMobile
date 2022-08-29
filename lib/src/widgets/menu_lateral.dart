40aa5be865194848b0dabf9860d8af022cbb1344
Sebastian <66321717+Chsebas98@users.noreply.github.com>
2022-08-29 10:46:26 -0500

    Menu Lateral Config

--- lib/src/widgets/menu_lateral.dart
+++ lib/src/widgets/menu_lateral.dart
@@ -5,49 +5,47 @@ import 'package:flutter/material.dart';
 import 'package:recipely/src/styles/styles.dart';
 
 import '../auth/screens/signin_screen.dart';
+import '../auth/utils/color_utils.dart';
 
 Drawer menuLateral(context) {
   return Drawer(
       elevation: 0.0,
       child: Container(
-          color: colorBG,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            hexStringToColor("CB2B93"),
+            hexStringToColor("9546C4"),
+            hexStringToColor("5E61F4")
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: ListView(padding: EdgeInsets.zero, children: [
             DrawerHeader(
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/receta_menu.png"),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      hexStringToColor("CB2B93"),
+                      hexStringToColor("5E61F4"),
+                    ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/logo.png"),
                         fit: BoxFit.cover)),
                 child: Container(
                     alignment: Alignment.center,
                     child:
                         const Text("RECIPELY", style: estiloTextoMenuLateral))),
-            ListTile(
-              leading: const Icon(Icons.fastfood, color: colorIcons),
-              title: const Text("Categorias"),
-              onTap: () {
-                Navigator.pushNamed(context, "categoria");
-              },
-            ),
-            ListTile(
+
+            /*ListTile(
               leading: const Icon(Icons.help, color: colorIcons),
               title: const Text("Ayuda"),
               onTap: () {
                 Navigator.pushNamed(context, "categoria");
               },
-            ),
-            const SizedBox(height: 350.0),
-            ListTile(
-              leading:
-                  const Icon(Icons.account_circle_outlined, color: colorIcons),
-              title: const Text("USUARIO"),
-              onTap: () {
-                Navigator.pushNamed(context, "categoria");
-              },
-            ),
-            const SizedBox(height: 20.0),
+            ),*/
+            const SizedBox(height: 440.0),
             ListTile(
               leading: const Icon(Icons.logout_outlined, color: colorIcons),
-              title: const Text("Cerrar Sesión"),
+              title: const Text(
+                "Cerrar Sesión",
+                style: TextStyle(color: Colors.white60, fontSize: 17.0),
+              ),
               onTap: () {
                 //cerrar sesion de firebase
                 FirebaseAuth.instance.signOut().then((value) {
@@ -58,6 +56,19 @@ Drawer menuLateral(context) {
                           builder: (context) => const SignInScreen()));
                 });
               },
-            )
+            ),
+            ListTile(
+                title: Row(children: [
+              Icon(
+                Icons.check_circle_outline_rounded,
+                color: Colors.blueGrey.shade300,
+                size: 14.0,
+              ),
+              Text(
+                "Versión 1.0",
+                style:
+                    TextStyle(fontSize: 10.0, color: Colors.blueGrey.shade300),
+              ),
+            ])),
           ])));
 }
